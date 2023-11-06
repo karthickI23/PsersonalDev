f8eebf4770991394e19bcb945dc49e4e37f9cc26
karthickI23 <karthick.i0323@gmail.com>
2023-11-06 14:48:26 +0530

    issue fix

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
 const parent = React.createElement('div', { id: 'parent' }, 'React Devlopment');
-const root = ReactDOM.createroot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent)
